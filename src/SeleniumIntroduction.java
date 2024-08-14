fda147b72794c524db0378aa524a1f9c664f00c7
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-08-14 15:07:54 +0530

    Run the same tests in Mozilla Firefox browser

--- src/SeleniumIntroduction.java
+++ src/SeleniumIntroduction.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumIntroduction {
 
@@ -14,24 +15,27 @@ public class SeleniumIntroduction {
 		//can focus on both WebDriver methods + class methods
 
 		//Method 03-Selenium Manger Won't Handle
+		//System.setProperty("webdriver.chrome.driver", "D:\\Documents\\Career\\My Projects\\Selenium Projects\\01-Introduction-Project\\selenium webdriver\\ChromeDriver\\chromedriver.exe");
+
 		//webdriver.chrome.driver->value of path
-		System.setProperty("webdriver.chrome.driver", "D:\\Documents\\Career\\My Projects\\Selenium Projects\\01-Introduction-Project\\selenium webdriver\\ChromeDriver\\chromedriver.exe");
-		
-		WebDriver driver = new ChromeDriver();
-		
+		//WebDriver driver = new ChromeDriver();
+
+		//Firefox Launch
+		//webdriver.gecko.driver
+		System.setProperty("webdriver.gecko.driver", "D:\\Documents\\Career\\My Projects\\Selenium Projects\\01-Introduction-Project\\selenium webdriver\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+
 		driver.get("https://rahulshettyacademy.com");
-		
+
 		System.out.println("Title of the page is : "+driver.getTitle());
 		System.out.println("Current URL of the page is : "+driver.getCurrentUrl());
-		
+
 		//only close the current window 
 		driver.close();
 		//close all the windows associated with current window
 		driver.quit();
-		
-		
-		
-		
+
+
 	}
 
 }
