f32ae338c66a573893f0d2395cda9141865d2dac
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-08-15 00:07:20 +0530

    Add css selector locators to identify the elements,use implicit timeouts and use selectorhub browser plugin to identify and validate the elements on the page

--- src/Locators.java
+++ src/Locators.java
@@ -1,3 +1,5 @@
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,6 +11,9 @@ public class Locators {
 		System.setProperty("webdriver.chrome.driver", "D:\\Documents\\Career\\My Projects\\Selenium Projects\\01-Introduction-Project\\selenium webdriver\\ChromeDriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
+		//implicit wait -5 seconds timeout until load the error message to the page and avoid cssSelector not found errors 
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		
 		//providing correct username and incorrect password to check whether it is indicating error message
@@ -17,6 +22,8 @@ public class Locators {
 		//<button class="submit signInBtn" type="submit">Sign In</button>
 		//there are two classes assigned for the sign in button among those two "signInBtn" class is more unique for that element
 		driver.findElement(By.className("signInBtn")).click();
+		//use cssSelector to get the error message due to wrong password
+		System.out.println("Error Log : "+driver.findElement(By.cssSelector("p.error")).getText());
 
 	}
 
