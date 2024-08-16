4153b29feed376521414be6a9fc186bb3179d49f
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-08-16 16:51:59 +0530

    Identify locators using siblings with xpath traverse

---
+++ src/Locators03.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators03 {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Documents\\Career\\My Projects\\Selenium Projects\\01-Introduction-Project\\selenium webdriver\\ChromeDriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+		//Sibling - Parent traverse
+		// /html/body/header <----absolute xpath - starting from the begining
+		// //header/div/button[1] <----relative xpath - starting from the header
+		// //header/div/button[1]/following-sibling::button[1]
+		System.out.println("Button text : "+driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+
+	}
+
+}
