5c290d35f74518d50b5744e019736e165c194fbe
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-08-17 11:43:31 +0530

    Completed the assignment 01

---
+++ src/Assignment01.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Assignment01 {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "D:\\Documents\\Career\\My Projects\\Selenium Projects\\01-Introduction-Project\\selenium webdriver\\ChromeDriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+		Assert.assertFalse(driver.findElement(By.id("checkBoxOption1")).isSelected());
+		driver.findElement(By.id("checkBoxOption1")).click();
+		Assert.assertTrue(driver.findElement(By.id("checkBoxOption1")).isSelected());
+		driver.findElement(By.id("checkBoxOption1")).click();
+		Assert.assertFalse(driver.findElement(By.id("checkBoxOption1")).isSelected());
+		System.out.println("Number of checkboxes in the page : "+driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+	}
+
+}
