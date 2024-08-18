56ba8dd0b4c9fce98ddedf3babfab208b75d7cad
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-08-18 15:38:18 +0530

    Implicit wait example

--- src/AddToCartSite.java
+++ src/AddToCartSite.java
@@ -1,3 +1,4 @@
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import org.openqa.selenium.By;
@@ -11,11 +12,27 @@ public class AddToCartSite {
 
 		System.setProperty("webdriver.chrome.driver", "D:\\Documents\\Career\\My Projects\\Selenium Projects\\01-Introduction-Project\\selenium webdriver\\ChromeDriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		//Add implicit wait to proper synchronization in script
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 
 		//create the array of items which needed to add to cart
 		//arraylist use more memory than arrays
 		String[] itemsNeeded = {"Cucumber","Brocolli","Beetroot"};
+		
+		addItems(driver,itemsNeeded);
+		
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+		driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+		driver.findElement(By.cssSelector("input[class='promoCode']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.xpath("//button[text()='Apply']")).click();
+		System.out.println("Promocode Applying Status : "+driver.findElement(By.cssSelector("span.promoInfo")).getText());
+		driver.findElement(By.xpath("//button[text()='Place Order']")).click();
+	}
+	
+
+	public static void addItems(WebDriver driver, String[] itemsNeeded) {
+		
 		//take all the web elements which contains product names
 		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 		//to stop the loop going extra iterations after find all items
@@ -36,6 +53,7 @@ public class AddToCartSite {
 				break;
 			}
 		}
+		
 	}
 
 }
