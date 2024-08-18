60f8588525761dac3e5c56fc20a14747a64a2dbd
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-08-18 22:43:12 +0530

    Fluent wait example

--- src/FluentWaitTest.java
+++ src/FluentWaitTest.java
@@ -17,23 +17,35 @@ public class FluentWaitTest {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
 
+		// Click the start button to initiate the dynamic content loading
 		driver.findElement(By.xpath("//div[@id='start']/button")).click();
 
-		Wait<WebDriver> wait = new FluentWait<>(driver).withTimeout(Duration.ofSeconds(30))
-							   .pollingEvery(Duration.ofSeconds(3)).ignoring(NoSuchElementException.class);
+		// Set up a FluentWait instance with a timeout of 30 seconds and polling interval of 3 seconds
+		Wait<WebDriver> wait = new FluentWait<>(driver)
+							   .withTimeout(Duration.ofSeconds(30))       // Maximum time to wait for the condition to be true
+				               .pollingEvery(Duration.ofSeconds(3))       // Check for the condition every 3 seconds
+				               .ignoring(NoSuchElementException.class);  // Ignore NoSuchElementException while waiting
 
+		// Define the condition for FluentWait - wait until the specified element is visible
 		WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
+			
 			public WebElement apply(WebDriver driver) {
+				// Locate the element that should appear after loading
 				WebElement element = driver.findElement(By.cssSelector("[id='finish'] h4"));
-				if(element.isDisplayed()) {
+
+				// If the element is displayed, return it; otherwise, return null to keep waiting
+				if (element.isDisplayed()) {
 					return element;
 				} else {
-					return null;  // Returning null will keep the wait looping until the timeout
+					return null;  // Returning null will continue the wait loop
 				}
 			}
+			
 		});
 
-		System.out.println("Final Text : "+foo.getText());  // Output the text once the element is found and displayed
+		// Output the text content of the element once it is found and displayed
+		System.out.println("Final Text : " + foo.getText());
 
+		// No need to quit the driver in this example, but in a real scenario, add driver.quit() to close the browser
 	}
 }
