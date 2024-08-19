c4c88a858434e895176910bb473856d01bd175c9
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-08-19 21:03:21 +0530

    Action class-real time example

--- src/ActionsDemo.java
+++ src/ActionsDemo.java
@@ -1,7 +1,10 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+
 
 public class ActionsDemo {
 
@@ -11,10 +14,16 @@ public class ActionsDemo {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.com/");
 		
-		// Create an instance of the Actions class, which allows us to perform complex user gestures
+		// Maximize the browser window for better visibility
+        driver.manage().window().maximize();
+        // Create an instance of the Actions class to perform complex user interactions
         Actions a = new Actions(driver);
-        // Move the mouse to the "Account & Lists" menu item by locating it via its ID, and perform the action
-        a.moveToElement(driver.findElement(By.id("nav-link-accountList"))).build().perform();
+        // Locate the "Account & Lists" menu item by its ID and store it in a WebElement
+        WebElement move = driver.findElement(By.id("nav-link-accountList"));
+        // Perform a mouse hover over the "Account & Lists" menu item
+        a.moveToElement(move).build().perform();
+        // Locate the search box by its ID, click on it, hold down the SHIFT key, and type "hello" in uppercase
+        a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").build().perform();
 
 	}
 
