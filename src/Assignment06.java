56b84ffc6af2db00c2fbf9cdd9cdc4a706d75f51
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-08-25 00:48:43 +0530

    Completed assignment 06

---
+++ src/Assignment06.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+public class Assignment06 {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Documents\\Career\\My Projects\\Selenium Projects\\01-Introduction-Project\\selenium webdriver\\ChromeDriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+		driver.findElement(By.id("checkBoxOption2")).click();
+		String checkboxText = driver.findElement(By.xpath("(//div[@id='checkbox-example']/fieldset/label)[2]")).getText();
+		driver.findElement(By.id("dropdown-class-example")).click();
+		WebElement staticDropdown = driver.findElement(By.id("dropdown-class-example")); 
+		Select dropdown = new Select(staticDropdown);
+		dropdown.selectByVisibleText(checkboxText);
+		driver.findElement(By.id("name")).sendKeys(checkboxText);
+		driver.findElement(By.id("alertbtn")).click();
+		String alertText = driver.switchTo().alert().getText();
+		System.out.println("alert text : "+alertText);
+		String extractedTextFromAlert = alertText.split(",")[0].split(" ")[1];
+		System.out.println("extracted alert text : "+extractedTextFromAlert);
+		Assert.assertEquals(checkboxText, extractedTextFromAlert);
+		driver.switchTo().alert().accept();
+
+	}
+
+}
