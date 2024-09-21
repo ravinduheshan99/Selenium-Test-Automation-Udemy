fd637e7ef700b9ca48e13f0749cc4cc8185ad6fd
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-09-21 22:47:38 +0530

    Explore chrome options to set proxies,plugins & paths on chrome browser

--- src/SSLCheck.java
+++ src/SSLCheck.java
@@ -1,25 +1,41 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.Proxy;  // Correct import
 
 public class SSLCheck {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// Setting up browser behaviors
-		// FirefoxOptions option2 = new FirefoxOptions();
-		// EdgeOptions option3 = new EdgeOptions();
-		ChromeOptions options = new ChromeOptions();
+        // Setting up ChromeOptions for browser behavior
+        ChromeOptions options = new ChromeOptions();
 
-		options.setAcceptInsecureCerts(true);
-
-		System.setProperty("webdriver.chrome.driver","D:\\Documents\\Career\\My Projects\\Selenium Projects\\01-Introduction-Project\\selenium webdriver\\ChromeDriver\\chromedriver.exe");
-		// Accept Insecured Sites
-		WebDriver driver = new ChromeDriver(options);
-		driver.get("https://expired.badssl.com/");
-		System.out.println("Page Title : " + driver.getTitle());
-	}
-
+        // Add the WebDriver proxy capability using Selenium's Proxy class
+        Proxy proxy = new Proxy();
+        proxy.setHttpProxy("myhttpproxy:3337");  // Define the proxy
+        options.setCapability("proxy", proxy);
+        
+        // To accept insecure SSL certs
+        options.setAcceptInsecureCerts(true);  
+        
+     
+        // These preferences allow us to customize the browser's behavior, such as changing the download directory.
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        // Set the browser's default download directory to a specified path.
+        // Replace "/directory/path" with the actual path where you want the files to be downloaded.
+        prefs.put("download.default_directory", "/directory/path");
+        // Apply the preferences to the ChromeOptions object.
+        // This is necessary for the WebDriver to use the custom preferences when launching the browser.
+        options.setExperimentalOption("prefs", prefs);
+        
+        System.setProperty("webdriver.chrome.driver", "D:\\Documents\\Career\\My Projects\\Selenium Projects\\01-Introduction-Project\\selenium webdriver\\ChromeDriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://expired.badssl.com/");
+        
+        System.out.println("Page Title : " + driver.getTitle());
+    }
 }
