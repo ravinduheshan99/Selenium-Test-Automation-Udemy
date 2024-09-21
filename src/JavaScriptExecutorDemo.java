cee0c20118a3027719f40e5a696aed80d138e50d
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-09-21 10:58:49 +0530

    How to handle table grids in webpage

--- src/JavaScriptExecutorDemo.java
+++ src/JavaScriptExecutorDemo.java
@@ -1,5 +1,9 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class JavaScriptExecutorDemo {
@@ -15,6 +19,13 @@ public class JavaScriptExecutorDemo {
 		Thread.sleep(3000);
 		js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
 		
+		List<WebElement> values =  driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+		int sum=0;
+		
+		for(WebElement element:values) {
+			sum = sum+Integer.parseInt(element.getText());
+		}
+		System.out.println("Total Amount Collected : "+sum);
 	}
 
 }
