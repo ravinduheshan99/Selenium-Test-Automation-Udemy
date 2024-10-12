7e1bcf12c5f66f05c2970dafd047ecc475d851fd
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-10-12 22:48:42 +0530

    How to take screenshots in selenium

--- src/Miscellneanous.java
+++ src/Miscellneanous.java
@@ -1,9 +1,15 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.commons.io.FileUtils;
 
 public class Miscellneanous {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		System.setProperty("webdriver.chrome.driver", "D:\\Documents\\Career\\My Projects\\Selenium Projects\\01-Introduction-Project\\selenium webdriver\\ChromeDriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -20,6 +26,16 @@ public class Miscellneanous {
         
         // Navigating to a specific webpage (in this case, Google's homepage)
         driver.get("http://google.com");
+        
+        // Capturing a screenshot of the current browser window
+        // TakesScreenshot is an interface that allows the WebDriver to take a screenshot
+        File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        
+        // Saving the screenshot to a specified file location on the disk
+        // Using FileUtils.copyFile to copy the screenshot to the desired path
+        FileUtils.copyFile(src, new File("D://Documents//Career//My Projects//Selenium Projects//01-Introduction-Project//Introduction//Screenshots//screenshot01.png"));
+        
+        // This will save the screenshot as "screenshot01.png" in the specified folder
 	}
 
 }
